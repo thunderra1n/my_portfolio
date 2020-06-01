befdb51be7243041fe8348f0f82dcf65ace5891c
ThunderRa1n <Podsn0w@gmail.com>
2020-06-01 20:30:09 +0900

    Updates

---
+++ precache-manifest.e15d7c1175c85bcec39e2192c6d9614f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "977b16954a5aca3c1981c18451b3cf4f",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "d3bf369d1e00af509cb1",
+    "url": "/my_portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f5e189e2ef7da36fdea7",
+    "url": "/my_portfolio/static/js/2.bd407200.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my_portfolio/static/js/2.bd407200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bf369d1e00af509cb1",
+    "url": "/my_portfolio/static/js/main.9de3571e.chunk.js"
+  },
+  {
+    "revision": "f471369dddfd9686ecce",
+    "url": "/my_portfolio/static/js/runtime-main.77f2245f.js"
+  }
+]);
